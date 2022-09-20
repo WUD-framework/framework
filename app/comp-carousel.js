e7adb27ed86e47dc4cd6e06a4f7ace3bb0f7bc81
anniesandberg <90192959+anniesandberg@users.noreply.github.com>
2022-09-20 13:49:16 +0200

    scss

--- app/comp-carousel.js
+++ app/comp-carousel.js
@@ -1,3 +1,5 @@
+const slideshowContainer = document.getElementById('slideshow-container');
+const numbertext = document.getElementsByClassName('numbertext')
 const mySlides = document.getElementsByClassName('mySlides');
 const next = document.getElementById('next');
 const prev = document.getElementById('prev');
@@ -13,30 +15,37 @@ function nextSlide(n) {
 
 function showSlides(n) {
     // Om siffran som räknas är större än antalet slides (=3) så skall räkningen börjar om från slide 1 igen
-    if (n > mySlides.length) {slideIndex = 1};
+    if (n > mySlides.length) {
+        slideIndex = 1
+    };
 
     // Om siffran som räknas är mindre än 1 skall slide 3 visas. Man går alltså från slide 1 till 3 när man trycke rpå prev-pilen
-    if (n < 1) {slideIndex = mySlides.length};
+    if (n < 1) {
+        slideIndex = mySlides.length
+    };
 
     // För varje slide, display = none
     for (let i = 0; i < mySlides.length; i++) {
-        mySlides[i].style.display = 'none'; 
+        mySlides[i].style.display = 'none';
     }
 
     //Display block för den slide vars index är aktuellt (minus 1, pga att räkningen börjar med noll per auto?)
-    mySlides[slideIndex-1].style.display = 'block';
+    mySlides[slideIndex - 1].style.display = 'block';
+    numbertext.textContent = 'test'
 
     console.log('slideIndex', slideIndex)
-    console.log('mySlides', mySlides[slideIndex-1])
+    console.log('mySlides', mySlides[slideIndex - 1])
     console.log('n', n)
+    console.log(slideIndex, '/', mySlides.length)
+
 }
 
 //Eventlyssnare för knapparna. Skicka in om 1 skall läggas till eller tas bort. 
-next.addEventListener('click', function(e) {
+next.addEventListener('click', function (e) {
     e.preventDefault();
     nextSlide(1);
 })
-prev.addEventListener('click', function(e){
+prev.addEventListener('click', function (e) {
     e.preventDefault();
     nextSlide(-1);
 })
