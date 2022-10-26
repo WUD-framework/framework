445f1ef4866abca162993718b85a61939e9cf087
CupcakeNemi <silje.straum@gmail.com>
2022-10-26 15:45:09 +0200

    started some masonry

--- app/comp-masonry.js
+++ app/comp-masonry.js
@@ -1,2 +1,21 @@
+// import Masonry from 'masonry-layout';
 
+// window.onload = () => {
+//     const grid = document.querySelector('.masonry');
+
+//     const masonry = new Masonry(grid);
+// }
+
+var elem = document.querySelector('.masonry-grid');
+var msnry = new Masonry( elem, {
+  // options
+    itemSelector: '.masonry-grid-item',
+    columnWidth: 200
+});
+
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.masonry-grid', {
+    // options
+});
 
